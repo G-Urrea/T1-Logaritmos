917746ce5599ed4ff564f14580cb576425b81928
G-Urrea <g.urrea.mansilla@gmail.com>
2022-10-01 00:20:47 -0300

    Test adaptado

--- test.c
+++ test.c
@@ -9,12 +9,7 @@
 typedef int (*func)(char*, char*, int);
 
 int main(){
-
-    func f1 = &alg1;
-    func f2 = &alg2;
-    benchmark(3, 15, 1, f1, "resultados1.txt");
-
-    benchmark(3, 15, 1, f2, "resultados2.txt");
-
+    func f[2] = {&alg1, &alg2};
+    benchmark(3, 12, 50, f, 2, "resultados.txt");
     return 0;
 }
